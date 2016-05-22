086e59a680823b251acf8b0a780b0b2256ce9f51
trololo <nicnl25@gmail.com>
2016-05-22 16:20:33 +0200

    Indentation en multiples de quatre + respect espacement python PEP 8

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-  return "Hello world!"
+    return "Hello world!"
+
 
 if __name__ == "__main__":
-  app.debug=True
-  app.run()
+    app.debug = True
+    app.run()
