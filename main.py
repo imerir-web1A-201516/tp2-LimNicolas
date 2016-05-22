1d862d1e199ab1f5fb18e8cbe48e2e70794514ad
trololo <nicnl25@gmail.com>
2016-05-22 16:25:56 +0200

    Liste de tous les étudiants

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+import students
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,6 +7,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello world!"
+
+
+@app.route("/students/list")
+def students_list():
+    return json.dumps(students.get_all_names())
 
 
 if __name__ == "__main__":
---
+++ students.py
@@ -0,0 +1,12 @@
+students = [
+  {"name": "John", "grades": [{"topic": "French", "mark": 12}, {"topic": "Math", "mark": 16}]},
+  {"name": "Jane", "grades": [{"topic": "French", "mark": 18}, {"topic": "Math", "mark": 9}]},
+  {"name": "Toto", "grades": [{"topic": "French", "mark": 8}, {"topic": "Math", "mark": 5}]}
+]
+
+
+def get_all_names():
+    output = []
+    for student in students:
+        output += [student['name']]
+    return output
